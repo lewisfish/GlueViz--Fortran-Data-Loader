a3d6a79986de39f7ac625a3aaccf5e9962467981
lewis mcmillan <lewistmcmillan@gmail.com>
2017-01-17 16:21:57 +0000

    Some minor changes plus added early support for changing 4 dim. still needs work...

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
-from glue.config import data_factory
+from glue.config import data_factory, link_function
 from glue.core import Data
+from glue.config import auto_refresh
 
 from qt_widget import QtFortranDialog
+
+auto_refresh(True)
+
 
 @data_factory('Import Fortran Unformatted File')
 def fort_import(name):
     wi = QtFortranDialog(name)
     wi.exec_()
-    return Data(cube=wi.X)
-    
-
+    return Data(x=wi.X)
--- qt_widget.py
+++ qt_widget.py
@@ -2,10 +2,11 @@ from glue.external.qt import QtGui
 from glue.external.qt.QtCore import Qt
 from glue.utils.qt.helpers import load_ui
 import numpy as np
-import os    
-    
-UI_MAIN = os.path.join(os.path.dirname(__file__), 'dialog.ui')    
-    
+import os
+
+UI_MAIN = os.path.join(os.path.dirname(__file__), 'dialog.ui')
+
+
 class QtFortranDialog(QtGui.QDialog):
 
     def __init__(self, name):
@@ -15,24 +16,30 @@ class QtFortranDialog(QtGui.QDialog):
         self.ui = load_ui(UI_MAIN, self)
         self.name = name
         self.X = []
-        self.valdim = 4
-        self.valtext = '4'
+        self.Y = []
+        self.Z = []
+        self.valdim = 3
+        self.valtext = '3'
         self.flag = False
-        
+        self.forthDim.hide()
+        self.forthval = 0
+
         self.Formattedbut.toggled.connect(self.hidelayout)
         self.Unformattedbut.toggled.connect(self.showlayout)
-        
+
         self.dimensions.valueChanged.connect(self.valuechange)
         self.GridSize.textChanged.connect(self.textchange)
         self.comboBox.activated[str].connect(self.selectionchange)
 
         self.Load.clicked.connect(self.click)
 
+        self.forthDim.valueChanged.connect(self.forthdimChange)
+
     def click(self):
 
         if not self.flag:
             fd = open(self.name, 'rb')
-            
+
             if '8' in self.valtext:
                 dt = np.float64
             elif '4' in self.valtext:
@@ -45,24 +52,26 @@ class QtFortranDialog(QtGui.QDialog):
                 ndim = self.valgrid
             except AttributeError:
                 self.GridSize.setFocus()
-            
+
             if magic == 4:
                 shape = (ndim, ndim, ndim, magic)
             elif magic == 3:
                 shape = (ndim, ndim, ndim)
             else:
                 print('Not yet supported...')
-            dat = dat.reshape(shape, order='F') 
+            dat = dat.reshape(shape)
             fd.close()
             if magic == 4:
-                dat = dat[:, :, :, 0]
+                dat = dat[:, :, :, self.forthval]
             self.X = dat[:, :, :]
+            # self.Y = dat[0, :, 0]
+            # self.Z = dat[0, 0, :]
             del dat
         else:
             self.X = np.loadtxt(self.name)
 
         self.accept()
-            
+
     def selectionchange(self, text):
         self.valtext = text
 
@@ -73,10 +82,19 @@ class QtFortranDialog(QtGui.QDialog):
             pass
         except AttributeError:
             self.GridSize.setFocus()
- 
+
     def valuechange(self):
         self.valdim = int(self.dimensions.value())
-        
+
+        if self.valdim == 4:
+            self.forthDim.show()
+        else:
+            self.forthDim.hide()
+
+    def forthdimChange(self):
+
+        self.forthval = int(self.forthDim.value())
+
     def hidelayout(self):
 
             self.dimensions.hide()
@@ -85,9 +103,9 @@ class QtFortranDialog(QtGui.QDialog):
             self.label.hide()
             self.label_2.hide()
             self.label_3.hide()
-            
+
             self.flag = True
-            
+
     def showlayout(self):
         if self.flag:
             self.dimensions.show()
@@ -96,7 +114,5 @@ class QtFortranDialog(QtGui.QDialog):
             self.label.show()
             self.label_2.show()
             self.label_3.show()
-            
+
             self.flag = False
-
-
